f21c3259f7f3941ab8165ee2c66932ca78338070
Alex Baranoff <baranoffalex7@gmail.com>
2022-04-05 20:36:44 -0600

    container work

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,14 @@
-import './App.css'
-import Header from './Components/Header/Header';
-import Slider from './Components/Slider/Slider';
-import Catalog from './Components/Catalog/Catalog';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Slider from "./Components/Slider/Slider";
+import Catalog from "./Components/Catalog/Catalog";
 function App() {
   return (
     <>
-      <Header />
-      <Slider />
+      <div id="page1">
+        <Header />
+        <Slider />
+      </div>
       <Catalog />
     </>
   );
