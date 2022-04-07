956226193c0bc19204b650144dd58e83c12fbb21
Alex Baranoff <baranoffalex7@gmail.com>
2022-04-06 20:42:09 -0600

    not sure. I want to get main updated, had to change item.js for some reason

--- website/src/Components/Catalog/Catalog.js
+++ website/src/Components/Catalog/Catalog.js
@@ -1,12 +1,9 @@
 import './Catalog.css'
-<<<<<<< HEAD
-=======
 import Item from './Items/Item';
->>>>>>> 8bd7f10 (big update)
 const Catalog = () => {
     return ( 
         <div id="catalog-container">
-
+            <Item />
         </div>
      );
 }
