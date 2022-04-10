7d0782c79538940163a2961d864a26c8ae8dda69
Alex Baranoff <baranoffalex7@gmail.com>
2022-04-09 21:58:56 -0600

    working total and some style

--- website/src/Components/Checkout/Checkout.js
+++ website/src/Components/Checkout/Checkout.js
@@ -1,12 +1,24 @@
 import "./Checkout.css";
-const Checkout = ({ items , handleCart }) => {
+const Checkout = ({ items, handleCart }) => {
   let cartItems = [];
-    handleCart(cartItems)
+  handleCart(cartItems);
+  let counter = 0;
+  let total = (cartItems) => {
+    for (let i = 0; i < cartItems.length; i++) {
+      counter = counter + cartItems[i].price * cartItems[i].qnt;
+    }
+    return counter;
+  };
   return (
     <div id="big">
+      <div id="total-container">total: {total(cartItems)}</div>
       {cartItems.map((item) => (
         <div className="cart-container" key={item.id}>
-          {item.name} {item.qnt} {item.price}
+          <div id="item-head">{item.name}</div>
+          <div id="price" style={{ color: "red" }}>
+            Price: {item.price}
+          </div>
+          Quantity: {item.qnt}
         </div>
       ))}
     </div>
